5f4c65038df45a1d7ed08a151aef2cf28f464f2d
Peter Corke <peter.i.corke@gmail.com>
2025-01-12 14:46:09 +1000

    working version, somewhat tidy code

--- sphinx_autorun/__init__.py
+++ sphinx_autorun/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-sphinxcontirb.autorun
+sphinxcontrib.autorun
 ~~~~~~~~~~~~~~~~~~~~~~
 
 Run the code and insert stdout after the code block.
@@ -10,8 +10,6 @@ Global options
 Add to your ``conf.py`` file::
 
     autorun_languages = {}
-    autorun_languages['pycon_output_encoding'] = 'UTF-8'
-    autorun_languages['pycon_input_encoding'] = 'UTF-8'
     autorun_languages['pycon_runfirst'] = '''
     lines of code to run before that included in the runblock
     this code does not appear in the output
@@ -20,14 +18,8 @@ Add to your ``conf.py`` file::
     np.set_printoptions(precision=4, suppress=True)
     '''
 
-Set ``pycon_input_encoding`` to UTF-8 if you use Unicode characters in the input file, since this means
-the text passed to ``autorun`` will be UTF-8 encoded
-Set ``pycon_output_encoding`` to UTF-8 if the output of the code in the runblock produces Unicode
-characters, for example ``ansitable``
-
 """
 import sys
-import os
 import io
 
 from code import InteractiveInterpreter
@@ -40,6 +32,120 @@ from sphinx.errors import SphinxError
 from sphinx_autorun import version
 
 __version__ = version.version
+
+
+class RunBlockError(SphinxError):
+    category = "runblock error"
+
+
+# Declare the autorun extension
+class AutoRun(object):
+    # default parameters, can be overriden by conf.py
+    config = {
+        "pycon": "python ",
+        "pycon_prefix_chars": 4,
+        "pycon_show_source": False,
+        "console": "bash",
+        "console_prefix_chars": 1,
+    }
+
+    @classmethod
+    def builder_init(cls, app):
+        cls.config.update(app.builder.config.autorun_languages)
+
+
+"""
+
+.. directivename:: arguments
+    :key1: val1
+    :key2: val2
+
+    directive content
+
+directive has arguments, options and content
+
+:role-name:`role content`
+
+"""
+
+
+# this class is related to the particular directive
+class RunBlock(Directive):
+    has_content = True
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {
+        "linenos": directives.flag,
+        "numpy": directives.flag,
+        "scipy": directives.flag,
+        "smtb": directives.flag,
+    }
+    print("Peter's version")
+
+    def run(self):
+        config = AutoRun.config  # dict of configurations from conf.py
+        language = self.arguments[0]  # first argument of the directive
+        # print(language)  # is pycon
+
+        if language not in config:
+            raise RunBlockError("Unknown language %s" % language)
+
+        # Get configuration values for the language from conf.py
+        args = config[language].split()
+        prefix_chars = config.get(language + "_prefix_chars", 0)
+        show_source = config.get(language + "_show_source", True)
+        runfirst = config.get(language + "_runfirst", None)
+
+        if "numpy" in self.options:
+            runfirst = "import numpy as np\n" + runfirst
+        if "scipy" in self.options:
+            runfirst = "import scipy as sp\n" + runfirst
+        if "smtb" in self.options:
+            runfirst = "from spatialmath import *\n"
+
+        # build the code block
+
+        # first add the runfirst code which comes from the configuration file, its a newline separated
+        # string without prompts.  This is not shown in the output
+        code = [rf.strip() for rf in runfirst.split("\n") if len(rf.strip()) > 0]
+
+        runfirst_len = len(code)
+
+        # self.content is a list of lines of the code block, with prompts
+        code += [line[prefix_chars:] for line in self.content]
+        # print(code)
+        results = runblock(code)
+        # print(results)
+
+        code_out = ""
+        for inp, outp in results[
+            runfirst_len:
+        ]:  # for all i/o pairs after the runfirst code
+            if "\n" in inp:
+                # multiline input
+                lines = inp.split("\n")
+                code_out += (
+                    "\n".join(
+                        [">>> " + lines[0]] + ["... " + line for line in lines[1:]]
+                    )
+                    + "\n"
+                )
+            else:
+                code_out += ">>> " + inp + "\n"
+            if len(outp) > 0:
+                code_out += outp + "\n"
+
+        literal = nodes.literal_block(code_out, code_out)
+        literal["language"] = language
+        literal["linenos"] = "linenos" in self.options
+        return [literal]
+
+
+def setup(app):
+    app.add_directive("runblock", RunBlock)
+    app.connect("builder-inited", AutoRun.builder_init)
+    app.add_config_value("autorun_languages", AutoRun.config, "env")
 
 
 def runsource(self, source, filename="<input>", symbol="single"):
@@ -64,6 +170,8 @@ def runsource(self, source, filename="<input>", symbol="single"):
     an exception is raised).  The return value can be used to
     decide whether to use sys.ps1 or sys.ps2 to prompt the next
     line.
+
+    THIS IS A MODIFIED VERSION OF THE FUNCTION FROM THE CODE MODULE
 
     """
     try:
@@ -107,122 +215,30 @@ def runsource(self, source, filename="<input>", symbol="single"):
 
 def runblock(code):
     print(code)
-    source_lines = (line[4:].rstrip() for line in code)
+    source_lines = (line.rstrip() for line in code)
     console = InteractiveInterpreter()
     results = []
 
     try:
         while True:
             source = next(source_lines)
-            # Allow the user to ignore specific lines of output.
-            if not source.endswith("# ignore"):
-                print(">>>", source)
+
             more, retval = runsource(console, source)
             while more:
                 next_line = next(source_lines)
                 # print("...", next_line)
                 source += "\n" + next_line
                 more, retval = runsource(console, source)
-            results.append((source.rstrip(), retval.rstrip()))
+
+            # lines of code included in the ReST file can be excluded from the final
+            # documentation if they end with a comment # ignore
+            if not source.endswith("# ignore"):
+                if retval is None:
+                    retval = "!! ^^^^^^^^ SYNTAX ERROR ^^^^^^^^"
+                results.append((source.rstrip(), retval.rstrip()))
     except StopIteration:
         if more:
             # print("... ")
             more, retval = runsource(console, source + "\n")
 
     return results
-
-
-class RunBlockError(SphinxError):
-    category = "runblock error"
-
-
-# this class is related to the sphinx extension
-class AutoRun(object):
-    here = os.path.abspath(__file__)
-    pycon = os.path.join(os.path.dirname(here), "pycon.py")
-    config = {
-        "pycon": "python " + pycon,
-        "pycon_prefix_chars": 4,
-        "pycon_show_source": False,
-        "console": "bash",
-        "console_prefix_chars": 1,
-    }
-
-    @classmethod
-    def builder_init(cls, app):
-        cls.config.update(app.builder.config.autorun_languages)
-
-
-"""
-
-.. directivename:: arguments
-    :key1: val1
-    :key2: val2
-
-    directive content
-
-directive has arguments, options and content
-
-:role-name:`role content`
-
-"""
-
-
-# this class is related to the particular directive
-class RunBlock(Directive):
-    has_content = True
-    required_arguments = 1
-    optional_arguments = 0
-    final_argument_whitespace = False
-    option_spec = {
-        "linenos": directives.flag,
-        "foobar": directives.unchanged,
-    }
-    print("Peter's version")
-
-    def run(self):
-        config = AutoRun.config
-        language = self.arguments[0]
-
-        if language not in config:
-            raise RunBlockError("Unknown language %s" % language)
-
-        # Get configuration values for the language
-        args = config[language].split()
-        input_encoding = config.get(language + "_input_encoding", "ascii")
-        output_encoding = config.get(language + "_output_encoding", "ascii")
-        prefix_chars = config.get(language + "_prefix_chars", 0)
-        show_source = config.get(language + "_show_source", True)
-        runfirst = config.get(language + "_runfirst", None)
-        runfirst = runfirst.strip().split("\n")
-
-        # self.content is a list of lines of the code block, with prompts
-        results = runblock(self.content)
-        # print(results)
-
-        code_out = ""
-        for inp, outp in results:
-            if "\n" in inp:
-                # multiline input
-                lines = inp.split("\n")
-                code_out += (
-                    "\n".join(
-                        [">>> " + lines[0]] + ["... " + line for line in lines[1:]]
-                    )
-                    + "\n"
-                )
-            else:
-                code_out += ">>> " + inp + "\n"
-            if len(outp) > 0:
-                code_out += outp + "\n"
-
-        literal = nodes.literal_block(code_out, code_out)
-        literal["language"] = language
-        literal["linenos"] = "linenos" in self.options
-        return [literal]
-
-
-def setup(app):
-    app.add_directive("runblock", RunBlock)
-    app.connect("builder-inited", AutoRun.builder_init)
-    app.add_config_value("autorun_languages", AutoRun.config, "env")
